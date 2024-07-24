45f3f64b0eb9a36320ae6e02cdbf839bb0eaa1d5
Jiyun <jiyumi00@gamil.com>
2024-07-24 16:32:43 +0900

    Lv1- ìã숫자 및 사직칙연산 입력받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,23 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner sc=new Scanner(System.in);
+        //숫자입력
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1=sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2=sc.nextInt();
+
+        //사칙연산 기호 입력
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator=sc.next().charAt(0);
+
+
+
     }
 }
